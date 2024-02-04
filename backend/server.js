293ded6ffebeb3dca2c506c820d317b85072c601
Satyajit Nayak <satyajitnayak348@gmail.com>
2024-02-04 13:53:55 +0530

    basic route & controller set

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,16 @@
-const dotenv = require('dotenv');
-const express = require('express');
+import dotenv from 'dotenv';
+import express from 'express';
+import authRoutes from "./routes/auth.routes.js"
 dotenv.config();
 
 const app = express();
-PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
     res.send("Yo bro! Anime is best");
 })
+
+app.use("/api/auth", authRoutes)
 
 app.listen(PORT, () => {
     console.log(`Server running: http://localhost:${PORT}`);
