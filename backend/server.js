60783c9dc9ab4bb1b7ffd489c4294f1462d1d67b
Satyajit Nayak <satyajitnayak348@gmail.com>
2024-02-04 20:17:42 +0530

    signup implemented

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,23 @@
+// Tutorial TIme: 42.35
+
 import dotenv from 'dotenv';
 import express from 'express';
 import authRoutes from "./routes/auth.routes.js"
+import connectToMongoDB from "./db/dbConnect.js";
+
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(express.json());
+app.use("/api/auth", authRoutes);
+
 app.get("/", (req, res) => {
     res.send("Yo bro! Anime is best");
-})
-
-app.use("/api/auth", authRoutes)
+});
 
 app.listen(PORT, () => {
+    // connectToMongoDB()
     console.log(`Server running: http://localhost:${PORT}`);
-})
+});
