34a4d9f633e752d390b1e8723a02ce6ca5558ea5
Satyajit Nayak <satyajitnayak348@gmail.com>
2024-02-05 15:27:26 +0530

    get users controller set

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,12 @@
 
 import dotenv from 'dotenv';
 import express from 'express';
-import authRoutes from "./routes/auth.routes.js"
 import cookieParser from 'cookie-parser';
+
+import authRoutes from "./routes/auth.routes.js"
 import messageRoutes from "./routes/messages.routes.js"
+import userRoutes from "./routes/user.routes.js"
+
 import connectToMongoDB from "./db/dbConnect.js";
 
 dotenv.config();
@@ -17,12 +20,13 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
 
 app.get("/", (req, res) => {
     res.send("Yo bro! Anime is best");
 });
 
 app.listen(PORT, () => {
-    connectToMongoDB()
+    // connectToMongoDB()
     console.log(`Server running: http://localhost:${PORT}`);
 });
