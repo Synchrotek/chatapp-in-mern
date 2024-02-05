90413c23a1f4ddfcecb0295fbd30eaace8028572
Satyajit Nayak <satyajitnayak348@gmail.com>
2024-02-05 15:15:54 +0530

    messageController done

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
-// Tutorial TIme: 42.35
+// Tutorial TIme: 1.10.00
 
 import dotenv from 'dotenv';
 import express from 'express';
 import authRoutes from "./routes/auth.routes.js"
+import cookieParser from 'cookie-parser';
+import messageRoutes from "./routes/messages.routes.js"
 import connectToMongoDB from "./db/dbConnect.js";
 
 dotenv.config();
@@ -11,13 +13,16 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(cookieParser());
+
 app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
 app.get("/", (req, res) => {
     res.send("Yo bro! Anime is best");
 });
 
 app.listen(PORT, () => {
-    // connectToMongoDB()
+    connectToMongoDB()
     console.log(`Server running: http://localhost:${PORT}`);
 });
