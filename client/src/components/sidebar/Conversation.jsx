37f93afd1cf89489296852d8a7d273c59f5b7bb8
Satyajit Nayak <satyajitnayak348@gmail.com>
2024-02-14 08:35:05 +0530

    Conversation props use

--- client/src/components/sidebar/Conversation.jsx
+++ client/src/components/sidebar/Conversation.jsx
@@ -1,25 +1,28 @@
 import React from 'react'
 
-const Conversation = () => {
+const Conversation = ({ conversation, emoji, lastIdx }) => {
     return (
         <>
             <div className='flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-1'>
 
                 <div className="avatar online">
                     <div className='w-12 rounded-full'>
-                        <img src="https://e7.pngegg.com/pngimages/799/987/png-clipart-computer-icons-avatar-icon-design-avatar-heroes-computer-wallpaper-thumbnail.png" alt="user avatar" />
+                        <img
+                            src={conversation.profilePic}
+                            alt="user avatar"
+                        />
                     </div>
                 </div>
 
                 <div className="flex flex-col flex-1">
                     <div className='flex gap-3 justify-between'>
-                        <p>User Name</p>
-                        <span className='text-xl'>🚀</span>
+                        <p>{conversation.fullName}</p>
+                        <span className='text-xl'>{emoji}</span>
                     </div>
                 </div>
 
             </div>
-            <div className='divider my-0 py-3 h-4' />
+            {!lastIdx && <div className='divider my-0 py-3 h-4' />}
         </>
     )
 }
